accdfaabba44ad5f14f967486b7feae22b1d7857
EDIR <lordride@gmail.com>
2016-08-12 16:42:28 -0300

    incluindo gitignore

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+        <meta name="description" content="Kiling Spree">
+        <meta name="keywords" content="DRAGONS,IMAGENS,MONSTERS">
+        <meta name="author" content="Edir R.M.">
+    <title>Kiling Spree</title>
+         <script type="text/javascript" src="script.js" ></script>
+            <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+
+
+
+</body>
+
+
+
+</html>
