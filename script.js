4eefea2c7c79ec3db681d97fa2d9655e22eaa559
EDIR <lordride@gmail.com>
2016-08-24 17:28:11 -0300

    data

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('#button').click(function() {
+    $('#button').click(function() {        
         $.get('http://localhost:63342/projectWIN/bomba.html', function (data) {
             $('.pagina').html(data);
 
