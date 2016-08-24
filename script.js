aae736c893c1181d35a9670b937ddebc0a5dc29f
EDIR <lordride@gmail.com>
2016-08-24 14:48:04 -0300

    melhorando

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
     $('#button').click(function() {
         $.get('http://localhost:63342/projectWIN/bomba.html', function (data) {
-            $('#pagina').html(data);
+            $('.pagina').html(data);
 
 
         });
     });
 });
+
