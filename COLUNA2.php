ae3c557bd3fc8e22d66af744e71592c9262a84c5
EDIR <lordride@gmail.com>
2016-08-29 17:32:57 -0300

    mudando tudo

---
+++ COLUNA2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CRONOMETRO</title>
+</head>
+<body>
+
+<form>
+    <div>
+        <input type="datetime-local" class="fimDoTeste" id="cronometro">
+    </div>
+</form>
+
+
+
+</body>
+</html>
---
+++ INICIO.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>INICIO</title>
+</head>
+<body>
+<form>
+    <div>
+        <input type="date" class="inicioDoTeste" id="relogio">
+    </div>
+</form>
+
+
+
+</body>
+</html>
