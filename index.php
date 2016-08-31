1a8838f2c96919500997d7c684c49797ef134c3f
EDIR <lordride@gmail.com>
2016-08-31 18:54:44 -0300

    mudando tudo de novo

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=3">
+    <meta name="description" content="Aprendendo">
+    <meta name="keywords" content="aprendendo,teste">
+    <meta name="author" content="Edir R.M.">
+
+    <title>Hello World</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <script type="text/javascript" src="js/jquery-3.1.0.js" ></script>
+    <script src="js/script.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+
+<body>
+        <figure id="img">
+            <img src="/icones/deathwing.jpg" alt="deathwing">
+        </figure>
+    <header><!--cabeçalho-->
+        <h1 id="titulo">Hello</h1>
+        <p>Welcome</p>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">Wallpappers</a>
+                    <a href="#">Images</a>
+                    <a href="#">Screan Shots</a>
+                    <a href="#">Video</a>
+                    <a href="#">Help</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+        <section id="main"><!--conteudo principal-->
+
+        </section>
+        <section id="destaques"><!--conteudo em destaque-->
+
+        </section>
+
+    <footer><!--rodape-->
+
+    </footer>
+</body>
+</html>
