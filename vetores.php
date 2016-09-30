a40ee9a28ca75304c69bb61d24f1954bffb0dd80
EDIR <lordride@gmail.com>
2016-09-30 15:30:59 -0300

    novidades

---
+++ vetores.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+<pre>
+<?php
+/*
+$vetor = array(5,8,9,6,4,7,1,2,3,4,8);
+//$vetor[] = 5;
+print_r($vetor);
+
+$par = 0;
+
+for ($i = 0; $i <= 10; $i++){
+    print_r($vetor[$i]);
+    if($vetor[$i] % 2 == 0){
+        $par = $par + 1;
+    }
+}
+
+print_r("<br>"."total de numeros pares é $par"."<br>");
+
+for ($i = 0; $i <=10; $i++){
+    if($vetor[$i] % 2 == 0){
+        print_r("posicao do numero par é $i"."<br>");
+    }
+}
+*/
+
+/*
+$c = range(5,20,5);
+foreach ($c as $v){
+    echo "$v"."<br>";
+}
+*/
+
+$nome = array("Paulo","Maria","Joana","Jessica");
+$nota1 = array(6.3,7.2,9.4,8.3);
+$nota2 = array(8.6,7.4,10.0,7.2);
+
+
+
+
+
+/*
+//---------------------------------------------------------------------
+COMANDO DE DESALOCA UM INDICE DO VETOR
+
+$n = array (...);
+unset($n[..]);
+
+
+//------------------------------------------------------------------------
+MODO DE CRIAR UM VETOR ESCOLHENDO O NUMERO DA POSICAO DO VALOR
+
+$v = array (1=>"A",3=>"B",6=>"C",8=>"D");
+
+//-----------------------------------------------------------------------------
+MODO DE CRIAR UM VETOR COM STRINGS
+
+$cadastro = array ("nome"=>"Ana",
+                    "peso"=>"68",
+                    "altura"=>"1.76");
+$cadastro["fuma"] = true;
+
+foreach ($cadastro as $campo => $valor){
+    echo "$campo  $valor"."<br>";
+}
+
+*/
+//------------------------------------------------------------------------------
+
+
+?>
+</pre>
+</body>
+</html>
