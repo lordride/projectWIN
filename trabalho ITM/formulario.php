5e729e73c0418727c0272faf72a8273662551f13
EDIR <lordride@gmail.com>
2016-10-05 08:45:54 -0300

    trabalho

--- trabalho ITM/formulario.php
+++ trabalho ITM/formulario.php
@@ -1,4 +1,14 @@
 <?php
+
+
+if(count($_POST)){
+
+
+
+}
+
+
+
 ?>
 <!DOCTYPE>
 <htmL>
@@ -7,41 +17,45 @@
 
     <link rel="stylesheet" type="text/css" href="/css/style.css">
 
-    <script type="text/javascript" src="/js/jquery-3.1.0.js" >
-
-    </script>
+    <script type="text/javascript" src="../js/jquery-3.1.0.js" ></script>
 </head>
 
 <body>
+
+
 <form method="post" action="funcoesFORMULARIO.php">
     <fieldset><legend>Cadastro Cliente</legend>
-        Nome:<br/><input type="text" name="nome" id="nomeCliente" placeholder="Nome"><br/>
-        CPF:<br/><input type="text" name="cpf" id="numCpf" placeholder="000.000.000-00">
-            <input type="button" value="Validar" id="botaoValidar" onclick=""><br/>
-        Data de Nascimento<br/><input type="date" name="dataNascimento" id="dataNasc"><br/>
+        Nome:<br/><input type="text" name="nome" id="nomeCliente" placeholder="Nome"><br/><br/>
+        CPF:<br/><input type="text" name="cpf" id="numCpf" placeholder="000.000.000-00" style="float: left">
+        <div style="float:left" class="infoCPF"> </div>
+        <input type="button" value="Validar" id="botaoValidar" onclick=""><br/>
+        <br/>Data de Nascimento<br/><input type="date" name="dataNascimento" id="dataNasc"><br/>
+
         <br/><input type="submit" value="Cadastrar" name="cadastro" id="botaoCadastro">
     </fieldset>
 </form>
-<form method="post" action="funcoesFORMULARIO.php">
-    <div style="border: 1px solid; text-align: center;">
 
-        <?php
+<script type="text/javascript">
+  $(function(){
 
-        include ('funcoesFORMULARIO.php');
+      $("#botaoValidar").click(function(){
 
-            if (isset($_POST['nome']) && isset($_POST['cpf']) && isset($_POST['dataNascimento'])){
-                $resultado = cadastro($_POST['nome'],$_POST['cpf'],$_POST['dataNascimento']);
-                echo "$resultado ";
-            }else{
-                echo "erro!";
-            }
+          var doc = $("#numCpf").val();
+
+          // validacao via ajax =>  ajax/index.php
+
+          // resposta da validacao coloca na tela
+
+          //$(".infoCPF").html();
+
+      });
+  })
 
 
 
-        ?>
-    </div>
-</form>
+</script>
+
+
 
 </body>
-
 </htmL>
